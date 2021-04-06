4547383c13ba97091f4b764981891c4d6c10b40a
LeonardoHdz93 <lhernandezm1909@alumno.ipn.mx>
2021-04-06 01:30:12 -0500

    curso formulario

---
+++ js/formulario.js
@@ -0,0 +1,21 @@
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', () => {
+		console.log('Tecla levantada');
+	})
+})
+
+formulario.addEventListener('submut', (e) => {
+	e.preventDefault();
+});
+
