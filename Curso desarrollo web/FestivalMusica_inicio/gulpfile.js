6f4296941a81f8fda85e267520672508ef23f85f
LeonardoHdz93 <lhernandezm1909@alumno.ipn.mx>
2021-06-16 14:58:06 -0500

    Festival de musica

--- Curso desarrollo web/FestivalMusica_inicio/gulpfile.js
+++ Curso desarrollo web/FestivalMusica_inicio/gulpfile.js
@@ -1,27 +1,61 @@
 const { series, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
+const concat = require('gulp-concat');
 
+
+const paths = {
+    imagenes:'src/img/**/*',
+    scss:'./src/scss/**/*.scss',
+    js:'src/js**/*.js'
+}
 // Funcion que compila SASS
 
 function css(){
-    return src('./src/scss/app.scss')
+    return src(paths.scss)
     .pipe(sass({
         outputStyle:'expanded'
     }))
     .pipe(dest('./build/css'));
 }
 function minimizarCSS(){
-    return src('./src/scss/app.scss')
+    return src(paths.scss)
     .pipe(sass({
         outputStyle:'compressed'
     }))
     .pipe(dest('./build/css'));
 }
+function javascript(){
+    return src(paths.js)
+    .pipe(concat('bundle.js'))
+    .pipe(dest('./build/js'));
+}
+function imagenes(){
+    return src(paths.imagenes)
+    .pipe(imagemin())
+    .pipe(dest('./build/img'))
+    .pipe( notify({message:'ImagenMinificada'}) );
+}
+function versionwebP(){
+    return src(paths.imagenes)
+    .pipe(webp())
+    .pipe(dest('./build/img'))
+    .pipe(notify({message:'Version webP lista'}));
+}
 function watchArchivos(){
-    watch('./src/scss/**/*.scss', css);
+    watch(paths.scss, css);
+    watch(paths.js,javascript);
 }
 
-exports.default = series(css)
+
+
+
 exports.css = css;
-exports.minimizarCSS = minimizarCSS
-exports.watchArchivos = watchArchivos
+exports.minimizarCSS = minimizarCSS;
+exports.imagenes = imagenes;
+exports.versionwebP = versionwebP;
+exports.watchArchivos = watchArchivos;
+exports.javascript = javascript;
+exports.default = series(css, imagenes,javascript,versionwebP, watchArchivos)
---
+++ Curso desarrollo web/FestivalMusica_inicio/src/js/app.js
@@ -0,0 +1,3 @@
+console.log('Listo!!');
+console.log('desde otro archivo ahvo prro');
++
